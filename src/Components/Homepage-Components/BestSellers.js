ef9dcd2f9c2f80a962c0b46369da32527128971b
Egnate Noniashvili <103636296+egnointhewoods@users.noreply.github.com>
2022-06-09 10:54:51 +0400

    Add Best Seller Products

--- src/Components/Homepage-Components/BestSellers.js
+++ src/Components/Homepage-Components/BestSellers.js
@@ -1,24 +1,41 @@
 import "../Homepage-Component-Styles/bestSellers.css";
+import useFetch from "../../Hooks/useFetch";
+import { useEffect, useState } from "react";
 
 export default function BestSellers() {
-  return (
-    <div className="bestSellersDiv">
-      <span>BEST SELLERS - </span>
-      <h2>Some of our most loved products.</h2>
-      <div className="productList">
-        <div className="productCard">
-          <h3>Item 1</h3>
-          <h4>Price: 20$</h4>
-        </div>
-        <div className="productCard">
-          <h3>Item 2</h3>
-          <h4>Price: 21$</h4>
-        </div>
-        <div className="productCard">
-          <h3>Item 3</h3>
-          <h4>Price: 200$</h4>
+  const { error, isLoading, data: items } = useFetch(
+    "https://dummyjson.com/products"
+  );
+
+  if (items) {
+    let trendingItems = [];
+    for (let i = 0; i < 4; i++) {
+      trendingItems.push(items.products[i]);
+    }
+    console.log(trendingItems);
+    return (
+      <div className="bestSellersDiv">
+        <span>BEST SELLERS - </span>
+        <h2>Some of our most loved products.</h2>
+        <div className="productList">
+          {trendingItems.map((item) => {
+            return (
+              <div className="productCard" key={item.id}>
+                <h3>{item.title}</h3>
+                <h3>{item.price}$</h3>
+                <img
+                  src="https://cdn.shopify.com/s/files/1/0068/9206/0724/files/Kits2_2000x.jpg?v=1650381101"
+                  alt="Product"
+                />
+              </div>
+            );
+          })}
         </div>
       </div>
-    </div>
-  );
+    );
+  } else if (error) {
+    return <>{error}</>;
+  } else if (isLoading) {
+    return <>Loading...</>;
+  }
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,6 +3,7 @@ export default function Navbarr() {
   return (
     <>
       <div className="header">FREE SHIPPING on orders over $45</div>
+
       <div className="nav">
         <input type="checkbox" id="nav-check" />
 
@@ -29,7 +30,7 @@ export default function Navbarr() {
         </div>
 
         <div className="nav-btn">
-          <label for="nav-check">
+          <label htmlFor="nav-check">
             <span></span>
             <span></span>
             <span></span>
@@ -48,6 +49,22 @@ export default function Navbarr() {
           <a href="/" target="_blank" rel="noreferrer">
             CART
           </a>
+        </div>
+        <div className="shopSubNav">
+          <div>
+            <a href="/" target="_blank" rel="noreferrer">
+              BANDAGES
+            </a>
+            <a href="/" target="_blank" rel="noreferrer">
+              HYDROCOLLOID
+            </a>
+            <a href="/" target="_blank" rel="noreferrer">
+              KITS
+            </a>
+            <a href="/" target="_blank" rel="noreferrer">
+              REFILLS
+            </a>
+          </div>
         </div>
       </div>
     </>
