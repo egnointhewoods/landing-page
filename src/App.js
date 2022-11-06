a6cd3f207622cb87935a93784758618841288f05
egnointhewoods <egnatenoniashvili6@gmail.com>
2022-11-06 20:08:01 +0000

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 import Homepage from "./Components/Homepage";
 import Details from "./Components/Details";
 import useCart from "./Hooks/useCart";
@@ -37,6 +38,7 @@ export default function App() {
           />
           <Route path="/collections" element={<Collections />} />
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
